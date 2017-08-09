9dc42c839e21daf8eb1e86d7fe2531e4510b0165
LogeshApp <30861642+LogeshApp@users.noreply.github.com>
2017-08-09 19:50:39 +0530

    Update Leap year or not.py

--- Leap year or not.py
+++ Leap year or not.py
@@ -1,19 +1,10 @@
 #Code for Leap year or not
 a=input("Enter the Year\n")
-b=a
-if(a%4==0):
-    if(a%100==0):
-        if(a%400==0):
-            a=1
-        else:
-            a=0
-    else:
-        a=1
+b=str (a)
+if(a%400==0 or (a%4==0 and a%100!=0)):
+    print( b + " is Leap Year")
 else:
-    a=0
-if (a==1):
-    print( str(b) + " is Leap Year")
-else:
-    print( str(b) + " is not a Leap")
+    print( b + " is not a Leap")
+
 
         
